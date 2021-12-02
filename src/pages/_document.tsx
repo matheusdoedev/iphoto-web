0fbbba674f5f3e6c0261a2c71c76d98c06f30edc
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-02 16:02:14 -0300

    feat: custom app and document pages file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,43 @@
+import { JSXElementConstructor, ReactElement, ReactFragment } from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<{
+    html: string;
+    head?: (JSX.Element | null)[] | undefined;
+    styles?:
+      | ReactElement<unknown, string | JSXElementConstructor<unknown>>[]
+      | ReactFragment
+      | undefined;
+  }> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700;800&family=Roboto&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
