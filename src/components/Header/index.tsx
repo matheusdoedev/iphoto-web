fc017651f7c9e3c0b580dcf3f6d391ad95bfcd1a
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-07 10:55:50 -0300

    refactor: Header component updated for user intern pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,41 @@
+import { memo } from 'react';
 import Image from 'next/image';
 
 import { Nav } from '~/components';
 
 import styles from './styles.module.scss';
 
-function Header(): JSX.Element {
+interface IHeaderProps {
+  internalPage?: boolean;
+}
+
+function Header({ internalPage }: IHeaderProps): JSX.Element {
   return (
-    <header className={styles.Header}>
+    <header
+      className={`${styles.Header} ${internalPage && styles.HeaderIntern}`}
+    >
       <Image
-        src="/assets/icons/iphoto-logo-white.svg"
+        src={`/assets/icons/iphoto-logo-${
+          internalPage ? 'black' : 'white'
+        }.svg`}
         alt="Iphoto"
         width={118}
         height={32}
         layout="fixed"
       />
-      <Nav />
+      <Nav
+        internalPage={internalPage}
+        userAvatarData={{
+          name: 'Mariah',
+          avatar: '/assets/images/avatar-mock.jpg',
+        }}
+      />
     </header>
   );
 }
 
-export default Header;
+Header.defaultProps = {
+  internalPage: false,
+};
+
+export default memo(Header);
