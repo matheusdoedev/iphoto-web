b6d46b65f18bea0b5d1604366732e84b5677de46
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-07 11:33:21 -0300

    refactor: working in user index page
    search bar, photos index and footer added

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,4 +1,7 @@
-import { Header } from '~/components';
+import Image from 'next/image';
+import { FiPlus } from 'react-icons/fi';
+
+import { Button, Footer, Header, Input } from '~/components';
 
 import styles from '~/styles/pages/user/index.module.scss';
 
@@ -7,7 +10,109 @@ function UserIndex(): JSX.Element {
     <section className={styles.UserIndex}>
       <div className={styles.UserIndexContainer}>
         <Header internalPage />
+        <section className={styles.UserSearchBar}>
+          <Input
+            name="search"
+            placeholder="Search photos and albums by name"
+            aria-label="Search By Name"
+          />
+          <Button style={{ maxWidth: '100%' }}>
+            <FiPlus size={16} color="FFF7ED" />
+            Upload photo
+          </Button>
+          <Button style={{ maxWidth: '100%' }}>
+            <FiPlus size={16} color="FFF7ED" />
+            Add new album
+          </Button>
+        </section>
+        <section className={styles.UserPhotos}>
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+          <Image
+            src="/assets/images/photo-mock.jpg"
+            alt="Foto"
+            width={419}
+            height={351}
+            layout="responsive"
+          />
+        </section>
       </div>
+      <Footer />
     </section>
   );
 }
