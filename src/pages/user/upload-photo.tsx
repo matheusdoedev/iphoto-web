c03a07890bbf0c7650508482e888b82fa96a1980
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-09 11:01:12 -0300

    feat: working in upload photo page

--- src/pages/user/upload-photo.tsx
+++ src/pages/user/upload-photo.tsx
@@ -1,10 +1,48 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import Image from 'next/image';
+import { useState } from 'react';
+
+import { Button, Input, InputMedia, Select } from '~/components';
+import { InternalPageLayout } from '~/layouts';
+import { IMedia, UpdatedLogo } from '~/models/Common';
+
+import styles from '~/styles/pages/user/upload-photo.module.scss';
+
 function UploadPhoto(): JSX.Element {
+  const [uploadPhoto, setUploadPhoto] = useState<UpdatedLogo>();
+
+  const handleChangeUploadPhoto = (fileObject: IMedia): void => {
+    setUploadPhoto(fileObject);
+  };
+
   return (
-    <section>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </section>
+    <InternalPageLayout
+      pageTitle="Upload a new photo"
+      pageDescription="Save a new photo"
+    >
+      <form className={styles.UploadPhotoForm}>
+        <label htmlFor="upload_photo" className={styles.UploadPhotoField}>
+          <Image
+            src="/assets/icons/camera-black.svg"
+            alt="Upload de Imagem"
+            width={53}
+            height={49}
+            layout="fixed"
+          />
+          <p>Upload new photo</p>
+          <InputMedia
+            name="upload_photo"
+            mediaType="image"
+            onChangeMedia={handleChangeUploadPhoto}
+          />
+        </label>
+        <Input name="title" label="Photo title" id="title" />
+        <Select name="album" label="Album" id="album" options={[]} />
+        <Button type="submit" style={{ maxWidth: '100%' }}>
+          Save photo
+        </Button>
+      </form>
+    </InternalPageLayout>
   );
 }
 
