c41210ef638feff3b512f70fabc61e7660554ec1
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-09 13:55:14 -0300

    refactor: App provider and react toastify config added to __App page file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { AppProps } from 'next/app';
 
+import { ToastContainer } from 'react-toastify';
+
+import AppProvider from '~/contexts';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import '~/styles/index.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </AppProvider>
+  );
 }
 
 export default MyApp;
