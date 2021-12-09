c7512a9efc916a692ca1b96a8c07142374963ee9
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-09 07:54:57 -0300

    feat: new layout for internal pages in user pages

---
+++ src/layouts/InternalPageLayout/index.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react';
+
+import { Footer, Header } from '~/components';
+
+import styles from './styles.module.scss';
+
+interface IInternalPageLayoutProps {
+  children: ReactNode;
+  pageTitle: string;
+  pageDescription?: string;
+}
+
+function InternalPageLayout({
+  children,
+  pageTitle,
+  pageDescription,
+}: IInternalPageLayoutProps): JSX.Element {
+  return (
+    <section className={styles.Wrapper}>
+      <div className={styles.Container}>
+        <Header internalPage />
+        <section className={styles.Content}>
+          <div className={styles.PageTitleBlock}>
+            <h1 className={styles.Title}>{pageTitle}</h1>
+            <p className={styles.Text}>{pageDescription}</p>
+          </div>
+          {children}
+        </section>
+      </div>
+      <Footer />
+    </section>
+  );
+}
+
+InternalPageLayout.defaultProps = {
+  pageDescription: undefined,
+};
+
+export default InternalPageLayout;
