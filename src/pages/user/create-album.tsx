2499a07c14a92955bc3e6e5d4d1fd4d4346af1da
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-09 07:55:13 -0300

    refactor: internal page layout implemented in create-album page

--- src/pages/user/create-album.tsx
+++ src/pages/user/create-album.tsx
@@ -1,33 +1,25 @@
-import { Button, Footer, Header, Input } from '~/components';
+import { Button, Input } from '~/components';
+import { InternalPageLayout } from '~/layouts';
 
 import styles from '~/styles/pages/user/create-album.module.scss';
 
 function CreateAlbum(): JSX.Element {
   return (
-    <section className={styles.CreateAlbum}>
-      <div className={styles.CreateAlbumContainer}>
-        <Header internalPage />
-        <section className={styles.CreateAlbumContent}>
-          <div className={styles.CreateAlbumPageTitleBlock}>
-            <h1 className={styles.CreateAlbumTitle}>Create new album</h1>
-            <p className={styles.CreateAlbumText}>
-              Create a new album to insert your photos in
-            </p>
-          </div>
-          <form className={styles.CreateAlbumForm}>
-            <Input
-              name="title"
-              label="Title"
-              placeholder="Insert the album title"
-            />
-            <Button type="submit" style={{ maxWidth: '100%' }}>
-              Create album
-            </Button>
-          </form>
-        </section>
-      </div>
-      <Footer />
-    </section>
+    <InternalPageLayout
+      pageTitle="Create new album"
+      pageDescription="Create a new album to insert your photos in"
+    >
+      <form className={styles.CreateAlbumForm}>
+        <Input
+          name="title"
+          label="Title"
+          placeholder="Insert the album title"
+        />
+        <Button type="submit" style={{ maxWidth: '100%' }}>
+          Create album
+        </Button>
+      </form>
+    </InternalPageLayout>
   );
 }
 
