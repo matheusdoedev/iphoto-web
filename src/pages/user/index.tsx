47933a5ab2377a926a1aec15ab9585180ea2e0d1
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-10 17:46:47 -0300

    feat: delete photo feature implemented

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,11 +1,10 @@
-import { useState, useEffect, useCallback, useMemo } from 'react';
-import Image from 'next/image';
+import { useState, useEffect, useCallback, useMemo, memo } from 'react';
 import { useRouter } from 'next/router';
 
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiTrash2 } from 'react-icons/fi';
 import { toast } from 'react-toastify';
 
-import { Button, Footer, Header, Input } from '~/components';
+import { Button, Footer, Header, Input, Modal } from '~/components';
 import { useAuthGuard } from '~/hooks';
 import { PhotoService } from '~/services';
 
@@ -16,6 +15,8 @@ function UserIndex(): JSX.Element {
   useAuthGuard();
 
   const [photos, setPhotos] = useState<IPhoto[]>();
+  const [confirmPhotoDeleteModal, setConfirmPhotoDeleteModal] = useState(false);
+  const [photoId, setPhotoId] = useState('');
 
   const photoService = useMemo(() => new PhotoService(), []);
   const router = useRouter();
@@ -38,18 +39,44 @@ function UserIndex(): JSX.Element {
     router.push('/user/upload-photo');
   };
 
+  const handleOpenDeletePhotoModal = (deletePhotoId: string): void => {
+    setPhotoId(deletePhotoId);
+    setConfirmPhotoDeleteModal(true);
+  };
+
+  const handleCloseModal = (): void => {
+    setConfirmPhotoDeleteModal(false);
+  };
+
+  const handleDeletePhoto = async (): Promise<void> => {
+    try {
+      await photoService.deletePhotoById(photoId);
+
+      handleGetUserPhotos();
+      setConfirmPhotoDeleteModal(false);
+      toast.success('Photo deleted.');
+    } catch (error) {
+      toast.error((error as Error).message);
+    }
+  };
+
   const PhotosMemo = useMemo(
     () =>
       photos && photos.length > 0 ? (
         <section className={styles.UserPhotos}>
           {photos.map((photo) => (
-            <Image
-              src={photo.url}
-              alt={photo.title}
-              width={419}
-              height={351}
-              layout="responsive"
-            />
+            <div className={styles.UserPhoto}>
+              <button
+                type="button"
+                className={styles.UserPhotoDeleteIcon}
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                onClick={() => handleOpenDeletePhotoModal(photo.id)}
+              >
+                <FiTrash2 size={24} color="#fff7ed" />
+              </button>
+              <img src={photo.url} alt={photo.title} />
+            </div>
           ))}
         </section>
       ) : (
@@ -58,39 +85,55 @@ function UserIndex(): JSX.Element {
     [photos],
   );
 
+  const ConfirmDeleteModalFooter = memo(() => (
+    <>
+      <Button type="button" size="small" secondary onClick={handleCloseModal}>
+        Cancel
+      </Button>
+      <Button type="button" size="small" onClick={handleDeletePhoto}>
+        Delete
+      </Button>
+    </>
+  ));
+
   useEffect(() => {
     handleGetUserPhotos();
   }, [handleGetUserPhotos]);
 
   return (
-    <section className={styles.UserIndex}>
-      <div className={styles.UserIndexContainer}>
-        <Header internalPage />
-        <section className={styles.UserSearchBar}>
-          <Input
-            name="search"
-            placeholder="Search photos and albums by name"
-            aria-label="Search By Name"
-          />
-          <Button
-            style={{ maxWidth: '100%' }}
-            onClick={handleNavigateToUploadPhoto}
-          >
-            <FiPlus size={16} color="FFF7ED" />
-            Upload photo
-          </Button>
-          <Button
-            style={{ maxWidth: '100%' }}
-            onClick={handleNavigateToCreateAlbum}
-          >
-            <FiPlus size={16} color="FFF7ED" />
-            Add new album
-          </Button>
-        </section>
-        {PhotosMemo}
-      </div>
-      <Footer />
-    </section>
+    <>
+      <section className={styles.UserIndex}>
+        <div className={styles.UserIndexContainer}>
+          <Header internalPage />
+          <section className={styles.UserSearchBar}>
+            <Input
+              name="search"
+              placeholder="Search photos and albums by name"
+              aria-label="Search By Name"
+            />
+            <Button size="fullSize" onClick={handleNavigateToUploadPhoto}>
+              <FiPlus size={16} color="FFF7ED" />
+              Upload photo
+            </Button>
+            <Button size="fullSize" onClick={handleNavigateToCreateAlbum}>
+              <FiPlus size={16} color="FFF7ED" />
+              Add new album
+            </Button>
+          </section>
+          {PhotosMemo}
+        </div>
+        <Footer />
+      </section>
+
+      <Modal
+        title="Delete photo"
+        modalIsVisible={confirmPhotoDeleteModal}
+        setModalIsVisible={setConfirmPhotoDeleteModal}
+        footer={<ConfirmDeleteModalFooter />}
+      >
+        <p>Are you sure that you want to delete that photo?</p>
+      </Modal>
+    </>
   );
 }
 
