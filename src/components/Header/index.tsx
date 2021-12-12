001ed06b8ad6606ec45e9b9193f102468fa936f6
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-12 20:36:48 -0300

    refactor: updating user info in header for dynamic data

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,18 @@ import { memo } from 'react';
 import Image from 'next/image';
 
 import { Nav } from '~/components';
+import { useAuth } from '~/contexts/AuthenticationContext';
 
 import styles from './styles.module.scss';
+import { ISignUpResponseUser } from '~/models/Authentication';
 
 interface IHeaderProps {
   internalPage?: boolean;
 }
 
 function Header({ internalPage }: IHeaderProps): JSX.Element {
+  const user = useAuth().user as ISignUpResponseUser;
+
   return (
     <header
       className={`${styles.Header} ${internalPage && styles.HeaderIntern}`}
@@ -26,8 +30,8 @@ function Header({ internalPage }: IHeaderProps): JSX.Element {
       <Nav
         internalPage={internalPage}
         userAvatarData={{
-          name: 'Mariah',
-          avatar: '/assets/images/avatar-mock.jpg',
+          name: user?.name,
+          avatar: '/assets/images/user-mock.png',
         }}
       />
     </header>
