875b0338fb7a35d689b919199a3675d7f56fa32a
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-20 16:16:16 -0300

    feat: Album service

---
+++ src/services/Album.ts
@@ -0,0 +1,30 @@
+import { AxiosPromise } from 'axios';
+
+import api from '~/config/api';
+import { IAlbum, ICreateAlbumDto } from '~/models/Album';
+import { IPageOptionsRequest, IPagination } from '~/models/Common';
+
+class AlbumService {
+  private baseUrl = 'albums';
+
+  private perPageDefault = 8;
+
+  postCreateAlbum(data: ICreateAlbumDto): AxiosPromise<IAlbum> {
+    return api.post(`${this.baseUrl}`, data);
+  }
+
+  getUserAlbums(
+    { page }: IPageOptionsRequest = {
+      page: 1,
+    },
+  ): AxiosPromise<IPagination<IAlbum[]>> {
+    return api.get(`${this.baseUrl}/user`, {
+      params: {
+        page,
+        perPage: this.perPageDefault,
+      },
+    });
+  }
+}
+
+export default AlbumService;
